b9e3a43cc0868aa9812fd8a2544b32f175325d19
Nicolle Gutierrez <130277462+nicsxxgutierrez@users.noreply.github.com>
2023-10-13 15:54:54 +0800

    main

--- src/views/manage.vue
+++ src/views/manage.vue
@@ -1,16 +1,37 @@
+
 <template>
-<v-container>
-Members: <br>
-Rontale, Dan Keneth M.<br>
-Rhigen Abaca <br>
-Nicolle Gutierrez
-</v-container>
+  <v-container>
+    Members:
+    <v-avatar class="member-avatar">
+      <img src="rontale.jpg" alt="Rontale" />
+    </v-avatar>
+    Rontale, Dan Keneth M.<br>
+
+    <v-avatar class="member-avatar">
+      <img src="rhigen.jpg" alt="Rhigen" />
+    </v-avatar>
+    Rhigen Abaca<br>
+
+    <v-avatar class="member-avatar">
+      <img src="nicolle.jpg" alt="Nicolle" />
+    </v-avatar>
+    Nicolle Gutierrez
+  </v-container>
 </template>
 
 <script>
-import { defineComponent } from 'vue';  
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'manage',
 });
 </script>
+
+<style>
+.member-avatar {
+  width: 100px; /* Adjust the size as needed */
+  height: 100px; /* Adjust the size as needed */
+  margin-right: 10px; /* Add some spacing between avatars */
+  display: inline-block;
+}
+</style>
